87352c72d34198bc8ef8b3befbade42dba926a5e
Pal Patel <palpatel16112@gmail.com>
2022-04-10 15:05:50 +0530

    created student model

--- Details/admin.py
+++ Details/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ArtStyle, Mentor
+from .models import ArtStyle, Mentor, Student
 
 # Register your models here.
 admin.site.register(ArtStyle)
 admin.site.register(Mentor)
+admin.site.register(Student)
--- Details/models.py
+++ Details/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 class ArtStyle(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
+    # name = models.CharField(max_length=50)
+    class Style(models.TextChoices):
+        PAINTING = 'PNT','Painting'
+        SCULPTURE = 'SCP','Sculpture'
+        POP_ART = 'PA','Pop_art'
+        SKETCHING = 'SKH','Sketching'
+        CRAFT = 'CFT','Craft'
+    style = models.CharField(max_length=30, choices=Style.choices)
 
     def __str__(self):
-        return self.name
+        return self.style
 
 class Mentor(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -16,7 +23,20 @@ class Mentor(models.Model):
     art_style = models.ForeignKey(ArtStyle, on_delete=models.CASCADE, related_name='art_style')
     contact_number = models.BigIntegerField(unique=True)
     email = models.EmailField(unique=True)
-    salary = models.BigIntegerField()
+
+    def __str__(self):
+        split_email = self.email.split('@')
+        return split_email[0]
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    art_style = models.ForeignKey(ArtStyle, on_delete=models.CASCADE, related_name='art_style')
+    contact_number = models.BigIntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    rank = models.IntegerField()
 
     def __str__(self):
         split_email = self.email.split('@')
