828cb53842b0845022c702611d50416fec32462d
Hetzie <hetvipatel1952@gmail.com>
2022-04-10 14:36:00 +0530

    created Mentor model

--- Details/admin.py
+++ Details/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ArtStyle
+from .models import ArtStyle, Mentor
 
 # Register your models here.
 admin.site.register(ArtStyle)
+admin.site.register(Mentor)
