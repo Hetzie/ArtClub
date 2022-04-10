29b85b7b357b64c7405c8fe3fbc9e79d816ae722
Hetzie <hetvipatel1952@gmail.com>
2022-04-10 14:30:24 +0530

    created Mentor model

--- Details/models.py
+++ Details/models.py
@@ -7,3 +7,17 @@ class ArtStyle(models.Model):
 
     def __str__(self):
         return self.name
+
+class Mentor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    art_style = models.ForeignKey(ArtStyle, on_delete=models.CASCADE, related_name='art_style')
+    contact_number = models.BigIntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    salary = models.BigIntegerField()
+
+    def __str__(self):
+        split_email = self.email.split('@')
+        return split_email[0]
